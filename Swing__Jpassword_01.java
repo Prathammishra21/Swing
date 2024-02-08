ccf7868cd13e8dd868e17b664613defdc5d05512
Pratham Mishra <mishrapratham119@gmail.com>
2024-02-08 23:19:09 +0530

    basic swing password

---
+++ Swing__Jpassword_01.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class Swing__Jpassword_01{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JPasswordField");
+       JPanel panel = new JPanel();
+        JLabel lb = new JLabel("Id");
+        JPasswordField pf = new JPasswordField(20);
+ 
+     panel.add(lb);
+     panel.add(pf);
+     frame.add(panel);
+       
+        frame.setSize(300,150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
